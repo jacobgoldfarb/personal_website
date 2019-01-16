36741406a925c3a9f458c95bf55561ada4cf0c6d
Jacob Goldfarb <jacobgoldfarb1997@gmail.com>
2019-01-15 23:14:04 -0500

    Initial comit>

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,39 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Home msg="Welcome to Your Vue.js App"/>
+        <img src="./assets/cartoon_self.png" alt="" style="">
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Home
   }
 }
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css?family=Roboto');
+@import url('https://fonts.googleapis.com/css?family=Work+Sans:400,500');
+
+body {
+    background: #E5E5E5;
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+font-family: 'Roboto', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+img {
+  width: 20%;
+  margin-top:4rem;
 }
 </style>
